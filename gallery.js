984b7e85e5bc643cd05a7f223c7f6298dbbb9fde
Prashant Dubey <dubeyprashant583@gmail.com>
2021-07-27 17:57:16 +0530

    gallery started

---
+++ gallery.js
@@ -0,0 +1,13 @@
+function showImage(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+  document.querySelector(".showImage").style.display = "block";
+}
+
+function closeImg() {
+  document.querySelector(".showImageContainer").style.display = "none";
+  document.querySelector(".showImage").style.display = "none";
+}
